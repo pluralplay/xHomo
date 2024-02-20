JeelsBoobz/mihomo

chore: add some fields for override

adapter/provider/parser.go
@@ -36,6 +36,8 @@ type OverrideSchema struct {
 	Interface      *string `provider:"interface-name,omitempty"`
 	RoutingMark    *int    `provider:"routing-mark,omitempty"`
 	IPVersion      *string `provider:"ip-version,omitempty"`
+	NamePrefix     *string `provider:"name-prefix,omitempty"`
+	NameSuffix     *string `provider:"name-suffix,omitempty"`
 }
 
 type proxyProviderSchema struct {

adapter/provider/provider.go
@@ -399,6 +399,14 @@ func proxiesParseAndFilter(filter string, excludeFilter string, excludeTypeArray
 				if override.IPVersion != nil {
 					mapping["ip-version"] = *override.IPVersion
 				}
+				if override.NamePrefix != nil {
+					name := mapping["name"].(string)
+					mapping["name"] = *override.NamePrefix + name
+				}
+				if override.NameSuffix != nil {
+					name := mapping["name"].(string)
+					mapping["name"] = name + *override.NameSuffix
+				}
 
 				proxy, err := adapter.ParseProxy(mapping)
 				if err != nil {

pluralplay/xHomo

adapter/provider/parser.go
@@ -28,14 +28,16 @@ type healthCheckSchema struct {
 }
 
 type OverrideSchema struct {
-	UDP            *bool   `provider:"udp,omitempty"`
-	Up             *string `provider:"up,omitempty"`
-	Down           *string `provider:"down,omitempty"`
-	DialerProxy    *string `provider:"dialer-proxy,omitempty"`
-	SkipCertVerify *bool   `provider:"skip-cert-verify,omitempty"`
-	Interface      *string `provider:"interface-name,omitempty"`
-	RoutingMark    *int    `provider:"routing-mark,omitempty"`
-	IPVersion      *string `provider:"ip-version,omitempty"`
+	UDP              *bool   `provider:"udp,omitempty"`
+	Up               *string `provider:"up,omitempty"`
+	Down             *string `provider:"down,omitempty"`
+	DialerProxy      *string `provider:"dialer-proxy,omitempty"`
+	SkipCertVerify   *bool   `provider:"skip-cert-verify,omitempty"`
+	Interface        *string `provider:"interface-name,omitempty"`
+	RoutingMark      *int    `provider:"routing-mark,omitempty"`
+	IPVersion        *string `provider:"ip-version,omitempty"`
+	AdditionalPrefix *string `provider:"additional-prefix,omitempty"`
+	AdditionalSuffix *string `provider:"additional-suffix,omitempty"`
 }
 
 type proxyProviderSchema struct {

adapter/provider/provider.go
@@ -399,6 +399,14 @@ func proxiesParseAndFilter(filter string, excludeFilter string, excludeTypeArray
 				if override.IPVersion != nil {
 					mapping["ip-version"] = *override.IPVersion
 				}
+				if override.AdditionalPrefix != nil {
+					name := mapping["name"].(string)
+					mapping["name"] = *override.AdditionalPrefix + name
+				}
+				if override.AdditionalSuffix != nil {
+					name := mapping["name"].(string)
+					mapping["name"] = name + *override.AdditionalSuffix
+				}
 
 				proxy, err := adapter.ParseProxy(mapping)
 				if err != nil {